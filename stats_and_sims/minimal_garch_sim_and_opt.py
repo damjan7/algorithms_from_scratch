66ce32a536c00c8e20ee3a2a975c94dfc7744e7f
damjan kostovic <71121178+damjan7@users.noreply.github.com>
2025-07-10 09:10:51 +0200

    Update minimal_garch_sim_and_opt.py

--- stats_and_sims/minimal_garch_sim_and_opt.py
+++ stats_and_sims/minimal_garch_sim_and_opt.py
@@ -1,8 +1,9 @@
 import numpy as np, scipy.optimize as opt
 
 def sim_garch(T, ω, α, β):
-    y = np.empty(T); h = np.empty(T)
-    h[0] = ω/(1-α-β); y[0] = np.sqrt(h[0])*np.random.randn()
+    y, h = np.empty(T), np.empty(T)
+    h[0] = ω/(1-α-β)
+    y[0] = np.sqrt(h[0])*np.random.randn()
     for t in range(1, T):
         h[t] = ω + α*y[t-1]**2 + β*h[t-1]
         y[t] = np.sqrt(h[t])*np.random.randn()
@@ -12,7 +13,8 @@ def nll(params, y):
     ω, α, β = params
     if ω<=0 or α<0 or β<0 or α+β>=1:        # constraints
         return 1e10
-    h = np.empty_like(y); h[0] = y.var()
+    h = np.empty_like(y)
+    h[0] = y.var()
     ll = 0.0
     for t in range(1, len(y)):
         h[t] = ω + α*y[t-1]**2 + β*h[t-1]
